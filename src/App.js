eb575c3b51e85d1fe563d9e09f1dbe6570ed9bf1
¨dim50dim¨ <zheliezkov@gmail.com>
2025-05-25 11:33:03 +0200

    one error - does not find Footer component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import About from './About';
 import './App.css';
 import Category from './Category';
+import Footer from './Footer';
 
 import Header from './Header';
 import Home from './Home';
@@ -13,6 +14,7 @@ function App() {
       <Home />
       <About />
       <Category />
+      <Footer/>
            </div>
   );
 }
