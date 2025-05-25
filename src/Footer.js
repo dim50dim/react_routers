f7fd6d227bb87ca89b1c4491c9998b7130696fa9
¨dim50dim¨ <zheliezkov@gmail.com>
2025-05-25 11:38:08 +0200

    Finally, all works .

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <div>
-            <footer>Footer</footer>
+            <footer><b>Footer</b></footer>
         </div>
     )
 }
